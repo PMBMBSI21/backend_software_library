1d02a9c7b8ba757eef21648f8e56c3243256f3a4
Wahyu <wahyubahri1212@gmail.com>
2022-01-20 14:08:23 +0700

    update software belum fix

--- api/controllers/software_controller1.go
+++ api/controllers/software_controller1.go
@@ -1,9 +1,7 @@
 package controllers
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"software_library/backend/api/models"
 	"software_library/backend/api/responses"
@@ -122,17 +120,33 @@ func (server *Server) UpdateSoftware(w http.ResponseWriter, r *http.Request) {
 		responses.ERROR(w, http.StatusBadRequest, err)
 		return
 	}
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		responses.ERROR(w, http.StatusUnprocessableEntity, err)
-		return
-	}
+	// body, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	responses.ERROR(w, http.StatusUnprocessableEntity, err)
+	// 	return
+	// }
 	software := models.Software{}
-	err = json.Unmarshal(body, &software)
-	if err != nil {
-		responses.ERROR(w, http.StatusUnprocessableEntity, err)
-		return
-	}
+	// err = json.Unmarshal(body, &software)
+	// if err != nil {
+	// 	responses.ERROR(w, http.StatusUnprocessableEntity, err)
+	// 	return
+	// }
+
+	software.Name = r.FormValue("Name")
+	kategori_id, _ := strconv.ParseUint(r.FormValue("KategoriID"), 10, 32)
+	software.KategoriID = uint32(kategori_id)
+	// software.ZipFile, _ = upload.UploadFile(w, r, "ZipFile", code)
+	software.LinkSource = r.FormValue("LinkSource")
+	software.LinkPreview = r.FormValue("LinkPreview")
+	software.LinkTutorial = r.FormValue("LinkTutorial")
+	software.License = r.FormValue("License")
+	software.Description = r.FormValue("Description")
+	// software.PreviewImage, _ = upload.UploadFile(w, r, "PreviewImage", code)
+	// software.Ebook, _ = upload.UploadFile(w, r, "Ebook", code)
+
+	productVersion, _ := strconv.ParseFloat(r.FormValue("ProductVersion"), 64)
+	software.ProductVersion = productVersion
+
 	updatedSoftware, err := software.UpdateSoftware(server.DB, uint32(uid))
 	if err != nil {
 		formattedError := formaterror.FormatError(err.Error())
